a287c94ea15d507eec54853d8884ec0394f8c2b8
Saurabh Totey <SaurabhTotey@gmail.com>
2022-01-16 00:51:51 -0700

    Hid usage of window as a background behind flag for easier debugging

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 use winit::{
 	event::{Event, WindowEvent},
 	event_loop::{ControlFlow, EventLoop},
-	platform::unix::WindowBuilderExtUnix,
-	platform::unix::XWindowType,
-	window::WindowBuilder,
+	platform::unix::{WindowBuilderExtUnix, XWindowType},
+	window::{Window, WindowBuilder},
 };
 
 fn main() {
+	let args = std::env::args().collect::<Vec<_>>();
 	env_logger::init();
 
 	let event_loop = EventLoop::new();
 	let monitor_size = event_loop.primary_monitor().unwrap().size();
-	let window = WindowBuilder::new()
-		.with_inner_size(monitor_size)
-		.with_x11_window_type(vec![XWindowType::Desktop])
-		.with_override_redirect(true)
-		.build(&event_loop)
-		.unwrap();
+	let window = if args.len() > 1 && args[1] == "BACKGROUND" {
+		WindowBuilder::new()
+			.with_inner_size(monitor_size)
+			.with_x11_window_type(vec![XWindowType::Desktop])
+			.with_override_redirect(true)
+			.build(&event_loop)
+			.unwrap()
+	} else {
+		Window::new(&event_loop).unwrap()
+	};
 
 	event_loop.run(move |event, _, control_flow| {
 		*control_flow = ControlFlow::Wait;
