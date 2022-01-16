9a4b8a5633314dcb77ba2ea787bb313cdaf50bf9
Saurabh Totey <SaurabhTotey@gmail.com>
2022-01-16 00:36:36 -0700

    Deleted unnecessary unmap and map code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use winit::{
 	event::{Event, WindowEvent},
 	event_loop::{ControlFlow, EventLoop},
 	platform::unix::WindowBuilderExtUnix,
-	platform::unix::WindowExtUnix,
 	platform::unix::XWindowType,
 	window::WindowBuilder,
 };
@@ -16,16 +15,6 @@ fn main() {
 		.with_override_redirect(true)
 		.build(&event_loop)
 		.unwrap();
-
-	// TODO: figure out if the below code (from https://stackoverflow.com/questions/45527720/how-can-i-make-a-window-override-redirect-with-glutin)
-	//  is necessary: we already have aoverride redirect
-	let x_display = window.xlib_display().unwrap() as *mut winit::platform::unix::x11::ffi::Display;
-	let x_window = window.xlib_window().unwrap() as winit::platform::unix::x11::ffi::XID;
-	let x_connection = std::sync::Arc::into_raw(window.xlib_xconnection().unwrap());
-	unsafe {
-		((*x_connection).xlib.XUnmapWindow)(x_display, x_window);
-		((*x_connection).xlib.XMapWindow)(x_display, x_window);
-	}
 
 	event_loop.run(move |event, _, control_flow| {
 		*control_flow = ControlFlow::Wait;
