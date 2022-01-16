1763648dc0db6ca1711de0dff570c5df575ae6e3
Saurabh Totey <SaurabhTotey@gmail.com>
2022-01-16 00:34:54 -0700

    Messed around with override-redirect and mapping and unmapping window onto display

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use winit::{
 	event::{Event, WindowEvent},
 	event_loop::{ControlFlow, EventLoop},
 	platform::unix::WindowBuilderExtUnix,
+	platform::unix::WindowExtUnix,
 	platform::unix::XWindowType,
 	window::WindowBuilder,
 };
@@ -12,8 +13,19 @@ fn main() {
 	let event_loop = EventLoop::new();
 	let window = WindowBuilder::new()
 		.with_x11_window_type(vec![XWindowType::Desktop])
+		.with_override_redirect(true)
 		.build(&event_loop)
 		.unwrap();
+
+	// TODO: figure out if the below code (from https://stackoverflow.com/questions/45527720/how-can-i-make-a-window-override-redirect-with-glutin)
+	//  is necessary: we already have aoverride redirect
+	let x_display = window.xlib_display().unwrap() as *mut winit::platform::unix::x11::ffi::Display;
+	let x_window = window.xlib_window().unwrap() as winit::platform::unix::x11::ffi::XID;
+	let x_connection = std::sync::Arc::into_raw(window.xlib_xconnection().unwrap());
+	unsafe {
+		((*x_connection).xlib.XUnmapWindow)(x_display, x_window);
+		((*x_connection).xlib.XMapWindow)(x_display, x_window);
+	}
 
 	event_loop.run(move |event, _, control_flow| {
 		*control_flow = ControlFlow::Wait;
