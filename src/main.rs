556d7cb448921cdaafaa9f5ab4d91ac0d65ea3d1
Saurabh Totey <SaurabhTotey@gmail.com>
2022-01-16 11:22:50 -0700

    Documentation

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use winit::event_loop::EventLoop;
 fn main() {
 	env_logger::init();
 
+	// Get whether this demo should be run in the background, which is determined by whether the user specified it as a
+	// command line argument. Default behaviour (for debugging purposes) is to assume the window won't be run in the
+	// background.
 	let args = std::env::args().collect::<Vec<_>>();
 	let is_background_window = args.len() > 1 && args[1] == "background";
 
+	// Create the window and let it run
 	let event_loop = EventLoop::new();
 	let demo_window =
 		pollster::block_on(window::DemoWindow::new(&event_loop, is_background_window));
--- src/window.rs
+++ src/window.rs
@@ -10,8 +10,14 @@ pub struct DemoWindow {
 }
 
 impl DemoWindow {
+	/**
+	 * Create a new DemoWindow.
+	 * Creating a background window assumes that X is being used.
+	 */
 	pub async fn new(event_loop: &EventLoop<()>, is_background: bool) -> Self {
 		let window = if is_background {
+			// Create a window for the background that isn't managed by window managers and that has the size of the
+			// primary monitor.
 			let monitor_size = event_loop.primary_monitor().unwrap().size();
 			WindowBuilder::new()
 				.with_inner_size(monitor_size)
@@ -20,11 +26,17 @@ impl DemoWindow {
 				.build(event_loop)
 				.unwrap()
 		} else {
+			// Just create a window since it's not the background; this is for debugging purposes and making things
+			// easier to run. Since I'm using a tiling window manager, I don't really care about the size.
 			Window::new(event_loop).unwrap()
 		};
 		return Self { window };
 	}
 
+	/**
+	 * Consume the DemoWindow and EventLoop and run management on the window.
+	 * While the window is open, this function is blocking.
+	 */
 	pub fn run(mut self, event_loop: EventLoop<()>) {
 		event_loop.run(move |event, _, control_flow| {
 			*control_flow = ControlFlow::Wait;
